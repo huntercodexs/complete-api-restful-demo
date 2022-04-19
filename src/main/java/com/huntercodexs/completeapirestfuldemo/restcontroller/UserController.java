ee4e760b2fbbe191f30cdfc9b651c86dfffb31df
Jereelton Teixeira <jereeltondevel@gmail.com>
2022-04-19 08:57:36 -0300

    Tested and working...

--- src/main/java/com/huntercodexs/completeapirestfuldemo/restcontroller/UserController.java
+++ src/main/java/com/huntercodexs/completeapirestfuldemo/restcontroller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @ControllerAdvice
 @RestController
+@RequestMapping("${api.prefix}")
 @CrossOrigin(origins = "*")
 public class UserController {
 
@@ -19,48 +20,54 @@ public class UserController {
     /**
      * Read User
      */
-    @GetMapping(path = "/api/users/{userid}")
+    @GetMapping(path = "/users/{userid}")
     public ResponseEntity<UserResponseDto> findOne(@PathVariable("userid") String userid) {
+        System.out.println("FindOne");
         return userService.findOne(userid);
     }
 
     /**
      * Read All Users
      */
-    @GetMapping(path = "/api/users")
+    @GetMapping(path = "/users")
     public ResponseEntity<UserResponseDto> findAll() {
+        System.out.println("FindAll");
         return userService.findAll();
     }
 
     /**
      * Create User
      */
-    @PostMapping(path = "/api/users")
+    @PostMapping(path = "/users")
     public ResponseEntity<UserResponseDto> create(@RequestBody UserRequestDto user) {
+        System.out.println("Create");
         return userService.create(user);
     }
 
     /**
      * Delete User
      */
-    @DeleteMapping (path = "/api/users/{userid}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @DeleteMapping (path = "/users/{userid}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<UserResponseDto> delete(@PathVariable("userid") String userid) {
+        System.out.println("Delete");
         return userService.delete(userid);
     }
 
     /**
      * Update User
      */
-    @PutMapping(path = "/api/users/{userid}")
+    @PutMapping(path = "/users/{userid}")
     public ResponseEntity<UserResponseDto> update(@PathVariable("userid") String userid, @RequestBody UserRequestDto user) {
+        System.out.println("Update");
         return userService.update(userid, user);
     }
 
     /**
      * Patch User
      */
-    @PatchMapping(path = "/api/users/{userid}")
+    @PatchMapping(path = "/users/{userid}")
     public ResponseEntity<UserResponseDto> patch(@PathVariable("userid") String userid, @RequestBody UserRequestDto user) {
+        System.out.println("Patcher");
         return userService.patch(userid, user);
     }
 
